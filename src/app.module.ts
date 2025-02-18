c85d6aa8d3f259c62df1e20e6c4cca324465918b
Erick Malan <erickjpk1234@gmail.com>
2025-02-18 09:48:16 -0500

    segundo commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 
 import { PrismaService } from './prisma/prisma.service';
 import { EmployeesModule } from './employees/employees.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [EmployeesModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // Aplica el middleware a todas las rutas
+  }
+}
